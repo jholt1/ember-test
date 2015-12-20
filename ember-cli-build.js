aa57254c43abdd8c6defddcd570aa528a7a4b889
jholt1 <jonathan.a.holt@gmail.com>
2015-12-20 21:05:29 +0000

    adding all

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import('bower_components/moment/moment.js');
+  app.import('bower_components/moment-timezone/builds/moment-timezone-with-data-2010-2020.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
